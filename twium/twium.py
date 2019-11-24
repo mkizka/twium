8791f76e4f7da989876d081f746aa9820bae3d40
Compeito <com0806peito@icloud.com>
2019-11-24 21:28:05 +0900

    インポート修正

--- twium/twium.py
+++ twium/twium.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from models import Tweet
+from twium.models import Tweet
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAX_SEARCH_COUNT = 200
